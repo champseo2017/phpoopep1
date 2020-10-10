04a586213bf2993e209aa2f7e6a9a0ea7be306dd
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2020-10-10 16:18:07 +0700

    ไม่จำเป็นต้องทราบชื่อล่วงหน้า ที่อยู่ใน __construct

--- class.product.php
+++ class.product.php
@@ -1,14 +1,26 @@
 <?php
 class product
 {
-     // $private $id;
-     // $private $name;
-     public function __construct($id, $name){
+    // $private $id;
+    // $private $name;
+    public function __construct($id, $name)
+    {
         $this->id = $id;
         $this->name = $name;
-     }
+    }
 
-     public function info() {
-         return $this->id. ":". $this->name;
-     }
+    public function info()
+    {
+        $info = "";
+        $lastProperty = end($this);
+        foreach ($this as $key => $value) {
+            if ($value == $lastProperty) {
+                $info .= $value;
+            } else {
+                $info .= $value . " : ";
+            }
+
+        }
+        return $info;
+    }
 }
