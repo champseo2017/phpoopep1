7b9ac449fa4a3a051620dc275ab574082ca37f3b
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2020-10-10 16:43:21 +0700

    สร้างดีสตรักเตอร์ (Destructor)

--- class.product.php
+++ class.product.php
@@ -1,26 +1,7 @@
 <?php
 class product
 {
-    // $private $id;
-    // $private $name;
-    public function __construct($id, $name)
-    {
-        $this->id = $id;
-        $this->name = $name;
-    }
-
-    public function info()
-    {
-        $info = "";
-        $lastProperty = end($this);
-        foreach ($this as $key => $value) {
-            if ($value == $lastProperty) {
-                $info .= $value;
-            } else {
-                $info .= $value . " : ";
-            }
-
-        }
-        return $info;
+    function __destruct() {
+        echo " Object Destroyed.";
     }
 }
