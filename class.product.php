7c9dd66f3220d72ecd78ec8cfa938288f46c8bff
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2020-10-10 14:44:34 +0700

    การสร้างคลาส

---
+++ class.product.php
@@ -0,0 +1,28 @@
+<?php
+class product
+{
+    private $id; // attribute
+    private $name; // attribute
+
+    public function getId()
+    { // getter method
+        return $this->id;
+    }
+
+    public function setId($id)
+    { // setter method
+        $this->id = $id;
+    }
+
+    public function getName()
+    { // getter method
+        return $this->name;
+    }
+
+    public function setName($name)
+    { // setter method
+        $this->name = $name;
+    }
+}
+
+
