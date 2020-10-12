c25b1a4c98d8690cccd36fd6978407a8390101ae
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2020-10-13 00:08:43 +0700

    คลาสไม่มีชื่อ
    ถ้าต้องการสร้าง class แบบเร็ว

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
-include 'class.product.php';
-include 'class.radio.php';
-$radio = new Radio("Tarnin");
-$radio->info();
+include "class.product.php";
+$object = new class {
+    public function hello($message){
+        return "Hello $message";
+    }
+};
+
+echo $object->hello('PHP7');
