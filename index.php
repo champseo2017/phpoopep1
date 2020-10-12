9b0538d3c7252174ada2c88a10d879ec0aeb32d8
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2020-10-12 22:43:52 +0700

    การสืบทอด
    การสืบทอด

--- index.php
+++ index.php
@@ -1,11 +1,7 @@
 <?php
 include 'class.product.php';
-$products = array(new Product("TV"), new Product("Radio"));
-foreach ($products as $key => $item) {
-    if ($key == 0) {
-        echo "<span>" . $item->info() . "</span>" . "<br>";
-    } else {
-        echo "<span>" . $item->info() . "</span>";
-    }
-
+include 'class.radio.php';
+$products = array(new Radio("Tarnin"), new Radio("Pana"));
+foreach($products as $item) {
+    echo $item->info();
 }
