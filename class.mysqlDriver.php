9dd129460a6c901f00f40a39df465e54b279af2e
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2020-10-13 16:42:26 +0700

    การ Implement
    เราจะอิมพลีเมนต์ (Implement) หรือเขียนให้สมบูรณ์ในคลาสนี้ ทุกโครงสร้างฟังก์ชันต้องเหมือนกับที่ได้นิยามไว้ในอินเทอร์เฟซ

--- class.mysqlDriver.php
+++ class.mysqlDriver.php
@@ -1,5 +1,10 @@
 <?php 
 include("interface.dbDriver.php");
 class MySQLDriver implements DBDriver {
-    
+    public function connect() {
+       // connect to database
+    }
+    public function execute($query){
+        // execute the query and output result
+    }
 }
